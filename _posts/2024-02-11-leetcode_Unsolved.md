36f6f93319336e45c3fe96e5655d84d62ace7170
egogit <egovici@naver.com>
2024-02-11 13:43:17 +0900

    post: unsolved problem list

---
+++ _posts/2024-02-11-leetcode_Unsolved.md
@@ -0,0 +1,19 @@
+---
+title: "[python3] Unsolved List"
+author: egogit
+date: '2024-02-11 13:14:00'
+categories:
+- Algorithm
+- LeetCode
+tags:
+- python
+- leetcode
+- unsolved
+---
+
+## 풀지 못한 문제 정리
+
+**DP(Hard)**
+
+ [LeetCode - 1463. Cherry Pickup II](https://leetcode.com/problems/cherry-pickup-ii/)
+
--- post.sh
+++ post.sh
@@ -12,20 +12,40 @@ if [ "${content}" == "leetcode" ] || [ "${content}" == "programmers" ]; then
     
     while [ ! $number ] || [ $number -le 0 ] ;
     do
-        read -p "Enter the number of quiz: " number
+        read -p "Enter the number of the quiz: " number
     done
     
     while [ ! "$lang" ] || [ "$lang" == "" ] ;
     do
-        read -p "Enter the language of solution(Python): " lang
+        read -p "Enter the language of the solution(Python): " lang
     done
     
-    template="./_posts/template/codingtest.md"
-    file="./_posts/${currentYear}-${currentMonth}-${currentDay}-${content}${number}_${lang}.md"
+    read -p "Is it unsolved problem?(Y/n): " unsolved
     
-    cp "${template}" "${file}"
-    echo "Successfully created: ${file}" 
+    unsolved=$(echo "$unsolved" | tr '[:upper:]' '[:lower:]')
     
+    if [[ "$unsolved" == "y" ]]; then
+        file="./_posts/2024-02-11-leetcode_Unsolved.md"
+        
+        read -p "Enter the algorithm type of the solution: " algorithm 
+        read -p "Enter the difficulty of the problem(Easy, Medium, Hard): " difficulty 
+        read -p "Enter the title of the problem: " title 
+        read -p "Enter the url of the problem: " url
+        
+        echo "**${algorithm}(${difficulty})**" >> "$file"
+        echo "" >> "$file"
+        echo "[${content} - ${number}. ${title}](${url})" >> "$file"
+        echo "" >> "$file"
+    
+    else
+        
+        template="./_posts/template/codingtest.md"
+        file="./_posts/${currentYear}-${currentMonth}-${currentDay}-${content}${number}_${lang}.md"
+        
+        cp "${template}" "${file}"
+    
+        echo "Successfully created: ${file}"
+    fi
     
 elif [ "${content}" == "FintechMan" ]; then
     number=0
@@ -41,7 +61,6 @@ elif [ "${content}" == "FintechMan" ]; then
     cp "${template}" "${file}"
     echo "Successfully created: ${file}" 
     
-    
 elif [ "${content}" == "Error" ]; then
     number=0
     
@@ -55,7 +74,6 @@ elif [ "${content}" == "Error" ]; then
     
     cp "${template}" "${file}"
     echo "Successfully created: ${file}" 
-    
     
 else
     read -p "Enter the name of post: " filename
